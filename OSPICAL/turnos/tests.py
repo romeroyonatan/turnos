ed6fa85a0f8d8a1ead75bf941eeed846666a4fbd
romeroyonatan <romeroyonatan@gmail.com>
2013-12-18 20:50:33 -0300

    Se agrego caso de fatiga

--- OSPICAL/turnos/tests.py
+++ OSPICAL/turnos/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from turnos.models import *
 from turnos.bussiness import *
+from django.utils import timezone
 
 class ReservarTurnoTest(TestCase):
     fixtures = ['turnos.json']
@@ -61,3 +62,30 @@ class ReservarTurnoTest(TestCase):
             self.b.reservarTurnos(afiliado_id, '12345678', listaTurnos)
         turno = Turno.objects.get(id=20)
         self.assertEqual(turno.estado, Turno.DISPONIBLE)
+        
+    def testFatiga(self):
+        afiliado_id = 1
+        listaTurnos = range(1000,2000)
+        for i in listaTurnos:
+            Turno.objects.create(fecha=timezone.now(),
+                                 estado=Turno.DISPONIBLE,
+                                 sobreturno=False,
+                                 consultorio=Consultorio.objects.get(id=1),
+                                 ee=EspecialistaEspecialidad.objects.get(id=1),
+                                 id = i,
+                                 )
+        reserva = self.b.reservarTurnos(afiliado_id, '12345678', listaTurnos)
+        lineas = LineaDeReserva.objects.filter(reserva = reserva)
+        self.assertIsNotNone(reserva)
+        self.assertEqual(reserva.afiliado.id, afiliado_id)
+        self.assertEqual(lineas.count(), len(listaTurnos))
+        for turno_id in listaTurnos:
+            turno = Turno.objects.get(id=turno_id)
+            historial = HistorialTurno.objects.latest('id')
+            self.assertEqual(turno.estado, Turno.RESERVADO)
+            self.assertIsNotNone(historial)
+            self.assertEqual(historial.estadoNuevo, turno.estado)
+        for lr in lineas:
+            self.assertEqual(lr.reserva.id, reserva.id)
+            self.assertIn(lr.turno.id, listaTurnos)
+            self.assertEqual(lr.estado, Turno.RESERVADO)
