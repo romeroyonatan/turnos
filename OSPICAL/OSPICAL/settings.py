14c718588c656785882ad16daef184574d001735
romeroyonatan <romeroyonatan@gmail.com>
2014-01-03 11:29:04 -0300

    Mejoras en las plantillas. Muestra el nombre de usuario en el footer.

--- OSPICAL/OSPICAL/settings.py
+++ OSPICAL/OSPICAL/settings.py
@@ -98,6 +98,7 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 TEMPLATE_CONTEXT_PROCESSORS = (
+   'django.contrib.auth.context_processors.auth',
    "django.core.context_processors.request",
    "django.core.context_processors.media",
    "django.contrib.messages.context_processors.messages"
