70963454aeb051ea0ceaf63584726bdd4a14f3c0
romeroyonatan <romeroyonatan@gmail.com>
2014-01-29 11:47:30 -0300

    Algunos cambios mas en el formulario de crear turnos

--- OSPICAL/turnos/static/js/turno.crear.js
+++ OSPICAL/turnos/static/js/turno.crear.js
@@ -1,16 +1,12 @@
-DATE_FORMAT = "%d-%m-%Y";
-
 Date.prototype.addDays = function(days) {
 	this.setDate(this.getDate()+days);
 }
-
 function cambiaDias() {
-	dias = $('#id_dias').val();
+	dias = parseInt($('#id_dias').val());
 	hasta = new Date();
-	hasta.addDays(dias - 1);
+	hasta.addDays(dias);
 	$('#id_hasta').text(hasta.toLocaleDateString());
 }
-
 $(document).ready(function(){
 	$('#id_dias').change(cambiaDias);
 	
